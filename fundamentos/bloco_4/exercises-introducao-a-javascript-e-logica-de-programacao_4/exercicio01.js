07f58c19a0a2336a24e762582489120980dcad3b
Josimar Schneider <josimarschneider@gmail.com>
2021-04-27 18:29:01 -0400

    questao 3

--- fundamentos/bloco_4/exercises-introducao-a-javascript-e-logica-de-programacao_4/exercicio01.js
+++ fundamentos/bloco_4/exercises-introducao-a-javascript-e-logica-de-programacao_4/exercicio01.js
@@ -3,10 +3,15 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-  
+  console.log('exercício 1');
   console.log('Bem-vinda, ' + info.personagem);
 
+  console.log('exercício 2');
   info.recorrente = 'sim';
-  //info.['recorrente] = 'sim';
-
+  //info.['recorrente] = 'sim'; // outro jeito de fazer]
   console.log(info);
+
+  console.log('exercício 3');
+  for (let key in info) {
+      console.log(key);
+  }

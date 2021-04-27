24c7222fc6cd0593694f53a198186601c4b4f458
Josimar Schneider <josimarschneider@gmail.com>
2021-04-26 22:32:28 -0400

    exercicio 2 bonus bloco_4_array e loop for

--- bloco_4/bonus_array.js
+++ bloco_4/bonus_array.js
@@ -11,4 +11,16 @@ for (let index = 1; index < array.length; index += 1) {
       }
     } 
 }
-console.log(array);
+console.log(array); // ordem crescente
+
+for (let index = 1; index < array.length; index += 1) {
+
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] > array[secondIndex]) {
+        position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;
+      }
+    } 
+}
+console.log(array); // ordem decrescente

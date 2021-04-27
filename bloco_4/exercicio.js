78bc6987c14a326c76d509f32e27a7c132dc6ad7
Josimar Schneider <josimarschneider@gmail.com>
2021-04-26 20:45:23 -0400

    exercicio2

--- bloco_4/exercicio.js
+++ bloco_4/exercicio.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let soma=0;
+/*//exercici01
 for (let index =0; index < numbers.length; index ++) {
     console.log(numbers[index]);
-};
+}*/
+//exercici02
+for (let index =0; index < numbers.length; index ++) {
+    console.log(soma=soma+numbers[index]);
+}
+
+
+

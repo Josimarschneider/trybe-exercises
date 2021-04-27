0d937ca2a3bad0538923712418e74e6d128bd912
Josimar Schneider <josimarschneider@gmail.com>
2021-04-26 20:57:00 -0400

    exercicio4

--- bloco_4/exercicio.js
+++ bloco_4/exercicio.js
@@ -7,8 +7,22 @@ for (let index =0; index < numbers.length; index ++) {
 //exercici02
 for (let index =0; index < numbers.length; index ++) {
     console.log(soma=soma+numbers[index]);
-}*/
+}
+//exercici03
 for (let index =0; index < numbers.length; index ++) {
     soma=soma+numbers[index];
 }
-  console.log(soma/numbers.length);
+  console.log(soma/numbers.length); */
+//exercici04
+  for (let index =0; index < numbers.length; index ++) {
+    soma=soma+numbers[index];
+}   
+  media = soma/numbers.length
+  if (media > 20){
+      console.log('valor maior que 20');
+  } else {
+      console.log('valor menor ou igual a 20');
+  }
+  
+
+
